3af5b24c004a95271ff11fe9e0dac9a3d7cbdc42
Jonathan <jonathan.sanchez.perez25@gmail.com>
2026-04-24 23:50:39 -0500

    Estructura unida a detalles de proyectos

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+function buttonClickDetailProject1() {
+    window.location.href = "/../page/proyecto-dashboard-shopify.html";
+}
+
+function buttonClickDetailProject2() {
+    window.location.href = "/../page/proyecto-salidas-grupales.html";
+}
+function buttonClickDetailProject3() {
+    window.location.href = "/../page/proyecto-tutorias-estudiantiles.html";
+}
