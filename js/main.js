c9a06506051a0eea3ecdfc63488a87920a25e31a
Jonathan <jonathan.sanchez.perez25@gmail.com>
2026-04-29 00:37:26 -0500

    Estructura unida a demo de proyectos

--- js/main.js
+++ js/main.js
@@ -6,6 +6,20 @@ function buttonClickDetailProject1() {
 function buttonClickDetailProject2() {
     window.location.href = "/../page/proyecto-salidas-grupales.html";
 }
+
 function buttonClickDetailProject3() {
     window.location.href = "/../page/proyecto-tutorias-estudiantiles.html";
+}
+
+
+function buttonClickDemoProject1() {
+    window.location.href = "/../demo/proyecto-dashboard-shopify/index.html";
+}
+
+function buttonClickDemoProject2() {
+    window.location.href = "/../demo/proyecto-salidas-grupales/index.html";
+}
+
+function buttonClickDemoProject3() {
+    window.location.href = "/../demo/proyecto-tutorias-estudiantiles/index.html";
 }
